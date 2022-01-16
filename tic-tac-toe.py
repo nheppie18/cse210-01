5c46de68acda0156a78694f02da79f54cb000c5c
nheppie18 <nh.123fchat@gmail.com>
2022-01-15 19:16:26 -0700

    finished tic tac toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,7 +2,8 @@
 Author: Nate Hepworth
 Tic-Tac-Toe Assignment
 """
-
+game_continues = True
+winner = None
 def make_board():
     board = []
     for tile in range(9):
@@ -28,35 +29,62 @@ def player_switch(player):
     elif player == "x":
         return "o"
 
-def winner(board):
+def check_winner(board):
+    global winner
+    global game_continues
     if (board[0] == board[1] == board[2]):
         winner = board[0]
+        game_continues = False
     elif (board[3] == board[4] == board[5]):
         winner = board[3]
+        game_continues = False
     elif (board[6] == board[7] == board[8]):
         winner = board[6]
+        game_continues = False
     elif (board[0] == board[3] == board[6]):
         winner = board[0]
+        game_continues = False
     elif (board[1] == board[4] == board[7]):
         winner = board[1]
+        game_continues = False
     elif (board[2] == board[5] == board[8]):
         winner = board[2]
+        game_continues = False
     elif (board[0] == board[4] == board[8]):
         winner = board[4]
+        game_continues = False
     elif (board[2] == board[4] == board[6]):
         winner = board[2]
-        return winner
+        game_continues = False
+    else:
+        winner = None
+        game_continues = True
+    return winner
+
+def check_if_game_end(board):
+    check_winner(board)
+    check_tie()
+count = 0
+def check_tie():
+    global game_continues
+    global count
+    count += 1
+    if count == 9:
+        game_continues = False
+        print("Tie!")
+    return
 
 def main():
-    player = player_switch("o")
+    player = player_switch("x")
     board = make_board()
-    count = 0
-    while count != 9 or not winner(board):
+    while game_continues:
+        player = player_switch(player)
         display_board(board)
         player_turn(player, board)
-        player = player_switch(player)
-        count += 1
+        check_if_game_end(board)
     display_board(board)
+    if winner != None:
+        print(f"Player {winner} has won this round!")
     print("Game Over")
 
 if __name__ == "__main__":
