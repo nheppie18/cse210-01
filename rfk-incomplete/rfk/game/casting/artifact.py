b6a3cd426200327f6d85d3cf4af5dc05d22f7002
nheppie18 <nh.123fchat@gmail.com>
2022-02-14 09:58:41 -0700

    working on game

---
+++ rfk-incomplete/rfk/game/casting/artifact.py
@@ -0,0 +1,13 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    def __init__(self):
+        pass
+
+    def get_position():
+        pass
+
+    def get_message():
+        pass
+    
